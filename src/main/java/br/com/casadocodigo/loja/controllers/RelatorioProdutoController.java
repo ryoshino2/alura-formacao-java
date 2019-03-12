c3d10e0484772dca39edf6e46e60dd9959c3a2e2
Rafael <ryoshino@uolinc.com>
2019-03-12 12:07:03 -0300

    Funcionando o relatorio geral, sem data

--- src/main/java/br/com/casadocodigo/loja/controllers/RelatorioProdutoController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/RelatorioProdutoController.java
@@ -4,10 +4,12 @@ import br.com.casadocodigo.loja.dao.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Calendar;
 import java.util.List;
 
 
@@ -24,4 +26,10 @@ public class RelatorioProdutoController {
         return produtos;
     }
 
+    @RequestMapping(value = "/{dataLancamento}", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Produto> findDate(@PathVariable("dataLancamento") Calendar dataLancamento){
+        List<Produto> produtos = (List<Produto>) produtoDAO.findDate(dataLancamento);
+        return produtos;
+    }
 }
